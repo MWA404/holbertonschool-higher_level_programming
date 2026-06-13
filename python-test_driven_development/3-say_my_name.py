2fbc62a5061d06f36b2e0caa3c78b07dcfa8ddfd
MWA404 <mmmo20148@gmail.com>
2026-06-13 20:29:45 +0300

    Add 3-say_my_name.py with tests

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module for say_my_name function.
+Prints a person's name.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints My name is <first_name> <last_name>.
+    Both arguments must be strings.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
