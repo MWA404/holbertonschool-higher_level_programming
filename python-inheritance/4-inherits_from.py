d292207025480028646435fd9f1af3b972ac7742
MWA404 <mmmo20148@gmail.com>
2026-06-25 13:40:20 +0300

    Fix pycodestyle in 4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,5 +3,5 @@
 
 
 def inherits_from(obj, a_class):
-    """Return True if obj inherited from a_class, but is not exactly a_class."""
+    """Check if obj inherited from a_class, not exactly a_class."""
     return isinstance(obj, a_class) and type(obj) is not a_class
