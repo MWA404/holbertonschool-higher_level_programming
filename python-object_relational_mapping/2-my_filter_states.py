4da139216a03b7f9e7ce71df35eac3fee8a31136
MWA404 <mmmo20148@gmail.com>
2026-08-13 11:50:23 +0300

    2. Filter states by user input

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Connects to MySQL and lists states matching a user-given name."""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                          user=username, passwd=password,
+                          db=db_name, charset="utf8")
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+        state_name)
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
