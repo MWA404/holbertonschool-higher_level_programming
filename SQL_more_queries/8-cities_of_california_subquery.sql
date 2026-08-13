8e3341b629db044bb029c0d808efefbb9e59132e
MWA404 <mmmo20148@gmail.com>
2026-08-13 10:05:42 +0300

    Use single quotes for string literal

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- lists all cities of California using a subquery, ordered by cities.id
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id ASC;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
