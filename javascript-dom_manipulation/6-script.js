a7d9c2968d4ac594b1714a245763557a8ab3c154
MWA404 <muhannadalraddadi@gmail.com>
2026-08-15 19:52:12 +0300

    6. Star wars character

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,8 @@
+fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    const character = document.querySelector('#character');
+    character.textContent = data.name;
+  });
