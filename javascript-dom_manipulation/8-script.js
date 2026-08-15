59c4efcb57b9e8b5f4e9ab7e26d6e61e272f3c86
MWA404 <muhannadalraddadi@gmail.com>
2026-08-15 19:52:46 +0300

    8. Say Hello!

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,12 @@
+function displayHello() {
+  fetch('https://hellosalut.stefanbohacek.com/?lang=fr')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      const hello = document.querySelector('#hello');
+      hello.textContent = data.hello;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', displayHello);
