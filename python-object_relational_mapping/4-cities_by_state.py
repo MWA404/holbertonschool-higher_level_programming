14733730148e16c7f2d4689a5e5505671e8c3f2f
MWA404 <mmmo20148@gmail.com>
2026-08-18 11:06:49 +0300

    4. Cities by states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -9,9 +9,14 @@ if __name__ == "__main__":
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", port=3306,
-                          user=username, passwd=password,
-                          db=db_name, charset="utf8")
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name,
+        charset="utf8"
+    )
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name "
                 "FROM cities JOIN states ON cities.state_id = states.id "
